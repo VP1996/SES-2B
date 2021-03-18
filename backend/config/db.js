85e711dbef867d06019f8244d515337c5af6942d
Mikaela McNamara <mcnamaramikaela@gmail.com>
2021-03-18 18:54:25 +1100

    updated backend to mongo atlas (cloud)

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectdb = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log('mongo db connected');
+	} catch (err) {
+		console.log('failed' + err);
+		// exit process with fail
+		process.exit(1);
+	}
+};
+
+module.exports = connectdb;
--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const PORT = 4000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('backend'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/multiauth', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
-const connection = mongoose.connection;
+const PORT = process.env.PORT || 5000;
 
-connection.once('open', function () {
-	console.log('MongoDB database connection established successfully');
-});
+app.listen(PORT, () => console.log(`server starting on port ${PORT}`));
 
-app.listen(PORT, function () {
-	console.log('Server is running on Port: ' + PORT);
-});
-
-app.get('/', (req, res) => {
-	res.send('SES 2B backend');
-});
+//connect database
+connectDB();
