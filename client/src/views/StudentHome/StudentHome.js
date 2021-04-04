be4785be188a49fa9934de1a74e719afb77b0a80
Krystianhuang <krystianhuang@gmail.com>
2021-04-04 11:31:58 +0800

    Show student timetable on student home page and add clicking hooks on the events

--- client/src/views/StudentHome/StudentHome.js
+++ client/src/views/StudentHome/StudentHome.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:client/src/views/StudentHome/StudentHome.js
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../../components/Navbar/Navbar';
@@ -13,3 +14,133 @@ const Student = () => {
 };
 
 export default Student;
+=======
+import React, {Component} from 'react';
+import Scheduler, {SchedulerData, ViewTypes} from 'react-big-scheduler';
+import withDragDropContext from './withDnDContext';
+import 'react-big-scheduler/lib/css/style.css';
+import NavBar from '../Navbar/Navbar';
+
+const DemoData = {
+    resources: [{
+        id: 'r1',
+        name: 'UTS-001'
+    }, {
+        id: 'r2',
+        name: 'UTS-002'
+    }],
+    events: [
+        {
+            id: 1,
+            start: '2021-04-04 09:30:00',
+            end: '2021-04-04 11:30:00',
+            resourceId: 'r1',
+            title: 'Software Development',
+            showPopover: true
+        },
+        {
+            id: 2,
+            start: '2021-04-04 12:30:00',
+            end: '2021-04-04 14:30:00',
+            resourceId: 'r2',
+            title: 'Database',
+            showPopover: true
+        }
+    ]
+}
+
+class Student extends Component {
+    constructor(props) {
+        super(props);
+
+        let schedulerData = new SchedulerData(new Date(), ViewTypes.Week, false, false, {
+            startResizable: false,
+            endResizable: false,
+            movable: false,
+            creatable: false,
+        });
+        schedulerData.localeMoment.locale('en');
+        schedulerData.setResources(DemoData.resources);
+        schedulerData.setEvents(DemoData.events);
+        schedulerData.config.schedulerWidth = '800';
+        this.state = {
+            viewModel: schedulerData
+        }
+    }
+
+    render() {
+        const {viewModel} = this.state;
+        return (
+            <div className='container'>
+                <NavBar/>
+                <Scheduler schedulerData={viewModel}
+                           prevClick={this.prevClick}
+                           nextClick={this.nextClick}
+                           onSelectDate={this.onSelectDate}
+                           onViewChange={this.onViewChange}
+                           eventItemClick={this.eventClicked}
+                           viewEventClick={this.ops1}
+                           viewEventText="Ops 1"
+                           viewEvent2Text="Ops 2"
+                           viewEvent2Click={this.ops2}
+                           toggleExpandFunc={this.toggleExpandFunc}
+                />
+            </div>
+
+        )
+    }
+
+    prevClick = (schedulerData) => {
+        schedulerData.prev();
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    nextClick = (schedulerData) => {
+        schedulerData.next();
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    onViewChange = (schedulerData, view) => {
+        schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    onSelectDate = (schedulerData, date) => {
+        schedulerData.setDate(date);
+        schedulerData.setEvents(DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    eventClicked = (schedulerData, event) => {
+        alert(`You just clicked an event: {id: ${event.id}, title: ${event.title}}, will jump to class detail page.`);
+    };
+
+    ops1 = (schedulerData, event) => {
+        alert(`You just executed ops1 to event: {id: ${event.id}, title: ${event.title}}`);
+    };
+
+    ops2 = (schedulerData, event) => {
+        alert(`You just executed ops2 to event: {id: ${event.id}, title: ${event.title}}`);
+    };
+
+    toggleExpandFunc = (schedulerData, slotId) => {
+        schedulerData.toggleExpandStatus(slotId);
+        this.setState({
+            viewModel: schedulerData
+        });
+    }
+}
+
+export default withDragDropContext(Student)
+>>>>>>> d9c5397a (Show student timetable will clicking event hooks.):client/src/components/Studenthome/Studenthome.js
