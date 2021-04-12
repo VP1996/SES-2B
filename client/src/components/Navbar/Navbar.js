d4d9f52257f071dae2531b988fe71d892c7c9b5a
Ayush Kanwal <53325312+ayushkanwal@users.noreply.github.com>
2021-04-12 13:57:12 +0530

    add login and registrations functions

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 const Navbar = () => {
-	return (
-		<div className='container'>
-			<nav className='navbar navbar-expand-lg navbar-light'>
-				<Link to='/' className='navbar-brand'>
-					AuthenticateUTS
-				</Link>
-				<div className='collpase navbar-collapse'>
-					<ul className='navbar-nav mr-auto'>
-						<li className='navbar-item'>
-							<Link to='/' className='nav-link'>
-								Home
-							</Link>
-						</li>
-						<li className='navbar-item'>
-							<Link to='/student-login' className='nav-link'>
-								Login
-							</Link>
-						</li>
-						<li className='navbar-item'>
-							<Link to='/teacher-login' className='nav-link'>
-								Sign up
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-			<br />
-		</div>
-	);
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <Link to="/" className="navbar-brand">
+          AuthenticateUTS
+        </Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/student-login" className="nav-link">
+                Student Login
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/teacher-login" className="nav-link">
+                Teacher login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <br />
+    </div>
+  );
 };
 
 export default Navbar;
--- client/src/views/StudentLogin/StudentLogin.js
+++ client/src/views/StudentLogin/StudentLogin.js
@@ -1,15 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import NavBar from '../../components/Navbar/Navbar';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../../components/Navbar/Navbar";
+
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 
 const StudentLogin = () => {
-	return (
-		<div className='container'>
-			<NavBar />
-			<h1>Testing routing:Student Login</h1>
-			<br />
-		</div>
-	);
+  return (
+    <div className="container">
+      <NavBar />
+      
+      <h1 class="font-weight-bold">Student log In</h1>
+      <container>
+       
+        <Row md={{ span: 6, offset: 6 }}>
+          <Col>
+            <Card bg={"light"} style={{ width: "18rem-" }}>
+              <Card.Body>
+                <Form
+                  action="/handle-login"
+                  method="post"
+                  class="was-validated"
+                >
+                  <Form.Group controlId="loginFormEmail">
+                    <Form.Label class="font-weight-bold">Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Enter Email"
+                      required
+                    />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group controlId="loginFormPassword">
+                    <Form.Label class="font-weight-bold">Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="Password"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="loginFormCheckbox">
+                    <Form.Check type="checkbox" label="ReCaptcha" required />
+                  </Form.Group>
+                  <button type="submit" class="btn btn-outline-info">
+                    Submit
+                  </button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </container>
+      <br />
+    </div>
+  );
 };
 
 export default StudentLogin;
