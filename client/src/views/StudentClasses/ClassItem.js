71a78a0fec1b4f85e11a78a99a6dfa17e3cbf16c
anjali kalambe <anjalikalambe@outlook.com.au>
2021-04-29 13:42:52 +1000

    Add Modal and Generate Pin for Email Authentication for Teacher and Student clients

--- client/src/views/StudentClasses/ClassItem.js
+++ client/src/views/StudentClasses/ClassItem.js
@@ -7,10 +7,27 @@ import RoundArrowsIcon from '../../images/roundArrowsIcon.png'
 import PinCodeIcon from '../../images/pinCodeIcon.png'
 import GreenTickIcon from '../../images/greenTickIcon.png'
 import { toast } from 'react-toastify';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+
 
 class ClassItem extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            show: false,
+            pin: '**********',
+            pinEntered: ''
+        };
+    }
+
+    handleChange = e => {
+        e.preventDefault();
+        this.setState({
+            pinEntered: e.target.value
+        })
     }
 
     render() {
@@ -25,7 +42,31 @@ class ClassItem extends Component {
                             {this.props.flags.facial && <img src={GreenTickIcon} />}
                             {!this.props.flags.captcha && <img src={RoundArrowsIcon}  onClick={()=>toast.error('Captcha check failed.')}/>}
                             {this.props.flags.captcha && <img src={GreenTickIcon} />}
-                            {!this.props.flags.pin &&  <img src={PinCodeIcon} onClick={()=>toast.success('Pin check was successful.')}/>}
+                            {!this.props.flags.pin && <img src={PinCodeIcon} onClick={() => this.setState({ show: true })} />}
+                            <Modal show={this.state.show} onHide={() => this.setState({ show: false })}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Enter a Pin</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>To be authenticated through this email-pin method, you must enter the pin emailed to you by your teacher. This will be your student email address with which your account was created.</Modal.Body>
+                                <Modal.Footer>
+                                    <InputGroup className="mb-3">
+                                        <FormControl
+                                            placeholder={this.state.pin}
+                                            aria-label={this.state.pin}
+                                            aria-describedby="basic-addon2"
+                                            value={this.state.pinEntered}
+                                            onChange={this.handleChange} 
+                                        />
+                                        <InputGroup.Append>
+                                            <Button variant="outline-secondary" onClick={this.onEnterPin}>Check pin</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+
+                                    <Button variant="primary" onClick={() => this.setState({ show: false, pinEntered: '' })}>
+                                        ok
+          </Button>
+                                </Modal.Footer>
+                            </Modal>
                             {this.props.flags.pin && <img src={GreenTickIcon} />}
                         </div>
                     </Card.Text>
--- client/src/views/TeacherClasses/ClassItem.js
+++ client/src/views/TeacherClasses/ClassItem.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'react-big-scheduler/lib/css/style.css';
 import './styles.css';
 import Card from "react-bootstrap/Card";
@@ -6,22 +6,71 @@ import CameraIcon from '../../images/cameraIcon.png'
 import RoundArrowsIcon from '../../images/roundArrowsIcon.png'
 import PinCodeIcon from '../../images/pinCodeIcon.png'
 import GreenTickIcon from '../../images/greenTickIcon.png'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import cryptoRandomString from 'crypto-random-string';
+
+
 class ClassItem extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            show: false,
+            pin: '**********',
+            pinOutput: ''
+        };
     }
 
+    onGeneratePin = e => {
+        e.preventDefault();
+        let string = cryptoRandomString({ length: 10 });
+        this.setState({
+            pinOutput: string
+        })
+    }
     render() {
         return (
-            <Card style={{ margin: '10px'}}>
+            <Card style={{ margin: '10px' }}>
                 <Card.Body>
                     <Card.Text>
                         <div className="class-name">{this.props.name}</div>
                         <div className="class-time">{this.props.startTime} - {this.props.endTime}</div>
                         <div className="class-button-group">
-                            {!this.props.flags.captcha && <img src={RoundArrowsIcon}  onClick={()=>alert('Go captcha page')}/>}
+                            {!this.props.flags.captcha && <img src={RoundArrowsIcon} onClick={() => alert('Go captcha page')} />}
                             {this.props.flags.captcha && <img src={GreenTickIcon} />}
-                            {!this.props.flags.pin &&  <img src={PinCodeIcon} onClick={()=>alert('Go pin page')}/>}
+                            {!this.props.flags.pin && <img src={PinCodeIcon} onClick={() => this.setState({ show: true })} />}
+
+                            <Modal show={this.state.show} onHide={() => this.setState({ show: false })}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Generate a Pin</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>To allow students to authenticate via Email, create a pin and email it to students. Students will then enter the pin, if correct, they will then be authenticated in this one way.</Modal.Body>
+                                <Modal.Footer>
+                                    <InputGroup className="mb-3">
+                                        <FormControl
+                                            placeholder={this.state.pin}
+                                            aria-label={this.state.pin}
+                                            aria-describedby="basic-addon2"
+                                            value={this.state.pinOutput}
+                                        />
+                                        <InputGroup.Append>
+                                            <Button variant="outline-secondary" onClick={this.onGeneratePin}>Show Pin</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+
+
+
+                                    <Button variant="secondary" onClick={() => this.setState({ show: false })}>
+                                        Email Class
+          </Button>
+                                    <Button variant="primary" onClick={() => this.setState({ show: false, pinOutput: '' })}>
+                                        Ok
+          </Button>
+                                </Modal.Footer>
+                            </Modal>
+
                             {this.props.flags.pin && <img src={GreenTickIcon} />}
                         </div>
                     </Card.Text>
