cf56c14eb40c9338fe761dc51fc4089967716fec
Mikaela McNamara <mcnamaramikaela@gmail.com>
2021-05-04 14:26:25 +1000

    Recaptcha backend verification
    Note: still need to set an expiration on the recaptcha, rn it expires super quick like 30sec - will update that soon

--- client/src/views/StudentClasses/ClassItem.js
+++ client/src/views/StudentClasses/ClassItem.js
@@ -1,38 +1,73 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'react-big-scheduler/lib/css/style.css';
 import './styles.css';
-import Card from "react-bootstrap/Card";
-import CameraIcon from '../../images/cameraIcon.png'
-import RoundArrowsIcon from '../../images/roundArrowsIcon.png'
-import PinCodeIcon from '../../images/pinCodeIcon.png'
-import GreenTickIcon from '../../images/greenTickIcon.png'
+import Card from 'react-bootstrap/Card';
+import CameraIcon from '../../images/cameraIcon.png';
+import RoundArrowsIcon from '../../images/roundArrowsIcon.png';
+import PinCodeIcon from '../../images/pinCodeIcon.png';
+import GreenTickIcon from '../../images/greenTickIcon.png';
 import { toast } from 'react-toastify';
+import Modal from '../../components/Interactiveauth/Interactive.js';
 
 class ClassItem extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			show: false,
+		};
+		this.showModal = this.showModal.bind(this);
+		this.hideModal = this.hideModal.bind(this);
+	}
 
-    render() {
-        return (
-            <Card style={{ margin: '10px'}}>
-                <Card.Body>
-                    <Card.Text>
-                        <div className="class-name">{this.props.name}</div>
-                        <div className="class-time">{this.props.startTime} - {this.props.endTime}</div>
-                        <div className="class-button-group">
-                            {!this.props.flags.facial && <img src={CameraIcon} onClick={()=>toast.success('You have successfully passed the facial check.')}/>}
-                            {this.props.flags.facial && <img src={GreenTickIcon} />}
-                            {!this.props.flags.captcha && <img src={RoundArrowsIcon}  onClick={()=>toast.error('Captcha check failed.')}/>}
-                            {this.props.flags.captcha && <img src={GreenTickIcon} />}
-                            {!this.props.flags.pin &&  <img src={PinCodeIcon} onClick={()=>toast.success('Pin check was successful.')}/>}
-                            {this.props.flags.pin && <img src={GreenTickIcon} />}
-                        </div>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        )
-    }
+	showModal = () => {
+		this.setState({ show: true });
+	};
+
+	hideModal = () => {
+		this.setState({ show: false });
+	};
+
+	render() {
+		return (
+			<Card style={{ margin: '10px' }}>
+				<Card.Body>
+					<Card.Text>
+						<div className='class-name'>{this.props.name}</div>
+						<div className='class-time'>
+							{this.props.startTime} - {this.props.endTime}
+						</div>
+						<div className='class-button-group'>
+							{!this.props.flags.facial && (
+								<img
+									src={CameraIcon}
+									onClick={() =>
+										toast.success(
+											'You have successfully passed the facial check.',
+										)
+									}
+								/>
+							)}
+							{this.props.flags.facial && <img src={GreenTickIcon} />}
+							<Modal show={this.state.show} handleClose={this.hideModal}>
+								<p>Modal</p>
+							</Modal>
+							{!this.props.flags.captcha && (
+								<img src={RoundArrowsIcon} onClick={() => this.showModal()} />
+							)}
+							{this.props.flags.captcha && <img src={GreenTickIcon} />}
+							{!this.props.flags.pin && (
+								<img
+									src={PinCodeIcon}
+									onClick={() => toast.success('Pin check was successful.')}
+								/>
+							)}
+							{this.props.flags.pin && <img src={GreenTickIcon} />}
+						</div>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		);
+	}
 }
 
 export default ClassItem;
